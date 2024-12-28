c3257e50e93f9d8bdbdcac7074b577ad27f5f3ee
Alex Kanellis <akan0033@student.monash.edu>
2024-12-28 13:38:39 +1100

    flat list of teams created

--- app/(tabs)/(home)/index.tsx
+++ app/(tabs)/(home)/index.tsx
@@ -1,15 +1,58 @@
 import { TeamCard } from "@/components/TeamCard";
 import { theme } from "@/theme";
-import { StatusBar } from "expo-status-bar";
-import { FlatList, StyleSheet, Text, View } from "react-native";
+import { router } from "expo-router";
+import { FlatList, StyleSheet } from "react-native";
+import { BaskitballButton } from "@/components/BaskitballButton";
 
 export default function App() {
   const teams = [
     {
-      id: 1,
-      name: "Vikings",
+      id: "1",
+      name: "Blackburn Vikings",
+      playerList: [],
+    },
+    {
+      id: "2",
+      name: "Nunnawading",
+      playerList: [],
+    },
+    {
+      id: "3",
+      name: "Sherbrooke",
       playerList: [],
     },
   ];
-  return <TeamCard team={teams}></TeamCard>;
+  return (
+    <FlatList
+      style={styles.container}
+      contentContainerStyle={styles.contentContainer}
+      data={teams}
+      renderItem={({ item }) => <TeamCard team={item}></TeamCard>}
+      ListEmptyComponent={
+        <BaskitballButton
+          title="Add your first team"
+          onPress={() => router.navigate("/newTeam")}
+        ></BaskitballButton>
+      }
+    ></FlatList>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: theme.colorWhite,
+  },
+  contentContainer: {
+    padding: 12,
+    shadowColor: theme.colorBlack,
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
+    elevation: 5,
+  },
+});
